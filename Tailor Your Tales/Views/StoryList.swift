2ad4d600de8212107b3da6c6ecaeef342ed157d3
Selah-Bell <bellscs@wwu.edu>
2024-05-15 11:50:55 -0700

    added scrolling grid view and style customization

--- Tailor Your Tales/Views/StoryList.swift
+++ Tailor Your Tales/Views/StoryList.swift
@@ -8,16 +8,58 @@
 import SwiftUI
 
 struct StoryList: View {
-    let stories = [nextweek, samstreasures, multiplechoices]
+    let stories = [nextweek, samstreasures, multiplechoices,nextweek, samstreasures, multiplechoices,nextweek, samstreasures, multiplechoices,nextweek, samstreasures, multiplechoices,nextweek, samstreasures, multiplechoices,multiplechoices,nextweek, samstreasures, multiplechoices,nextweek, samstreasures,multiplechoices,nextweek, samstreasures, multiplechoices,nextweek, samstreasures,multiplechoices,nextweek, samstreasures, multiplechoices,nextweek, samstreasures,multiplechoices,nextweek, samstreasures, multiplechoices,nextweek, samstreasures,multiplechoices,nextweek, samstreasures, multiplechoices,nextweek, samstreasures,multiplechoices,nextweek, samstreasures, multiplechoices,nextweek, samstreasures,multiplechoices,nextweek, samstreasures, multiplechoices,nextweek, samstreasures,multiplechoices,nextweek, samstreasures, multiplechoices,nextweek, samstreasures,]
+    let columns = [
+           GridItem(.adaptive(minimum: 250))
+       ]
     var body: some View {
-        List(stories) { story in
+        //Text("Tailor Your Tales").font(.title)
+        VStack{
+            Text("Tailor Your Tales")
+                //.font(.largeTitle)
+                .foregroundStyle(.purple)
+                //.font(.system(.body, design: .rounded))
+                //.fontWeight(.heavy)
+                .font(.custom("Helvetica Bold", size: 75)).padding(10)
+            
+            Text("Tap on a title to read a story!").font(.custom("Helvetica", size: 35)).padding(6)
+            ScrollView(.vertical){
+                LazyVGrid(columns: columns, alignment: .center, spacing: 75){
+                    ForEach(0 ..< stories.count) {story in
+                        NavigationLink {
+                            StoryView(story: stories[story], pageIndex: 0)
+                        } label: {
+                            StoryRow(story: stories[story])
+                        }
+                        .frame(width: 250, height: 250, alignment: .center)
+                        .cornerRadius(50)
+                        .background(.blue)
+                        .foregroundColor(.white)
+                        .font(.title)
+                        
+                        /*ForEach(0 ..< stories.count, id: \.self) {story in
+                         NavigationLink {
+                         StoryView(story: stories[story], pageIndex: 0)
+                         } label: {
+                         StoryRow(story: stories[story])
+                         }*//*.frame(width: 300, height: 300, alignment: .center)
+                             .background(.blue)
+                             
+                             
+                             .font(.title)*/
+                    }
+                }
+                
+            }
+        }//.navigationTitle("Tailor Your Tales").font(.largeTitle)
+        /*List(stories) { story in
             NavigationLink {
                 StoryView(story: story, pageIndex: 0)
             } label: {
                 StoryRow(story: story)
             }
         }
-        .navigationTitle("Stories")
+        .navigationTitle("Stories")*/
     }
 }
 
